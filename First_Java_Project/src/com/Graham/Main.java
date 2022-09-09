6aa0b38338efaa18b4e316a13588308e3cf26b0e
Abiodun14 <Bybrymo@gmail.com>
2022-09-09 23:53:09 +0100

    java project

---
+++ First_Java_Project/src/com/Graham/Main.java
@@ -0,0 +1,8 @@
+package com.Graham;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Africa is the future of the world");
+    }
+}
